951b1fad343eb05f235777e03b82fb2abccc17fe
Kamil Gatkowski <152792497+ekafrozen@users.noreply.github.com>
2024-05-26 21:01:37 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   alias                      = "localstack"
-  access_key                 = ""
-  secret_key                 = ""
+  access_key                 = "fake"
+  secret_key                 = "fake"
+  region                     = "eu-central-1"
   skip_credentials_validation = true
   skip_metadata_api_check    = true
   skip_requesting_account_id = true
