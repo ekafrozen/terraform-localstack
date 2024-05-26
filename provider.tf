5128a6ea97a18e1245ce6dcf23b9915c1a7ef728
Kamil Gatkowski <152792497+ekafrozen@users.noreply.github.com>
2024-05-26 20:50:19 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,10 @@
 provider "aws" {
   alias                      = "localstack"
+  access_key                 = ""
+  secret_key                 = ""
   skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
+  skip_metadata_api_check    = true
+  skip_requesting_account_id = true
 
   endpoints {
     s3 = "http://localhost:4566"
