d8ff9a8b5f402cf13483251ed5d4ca50b7a4e403
Kamil Gatkowski <152792497+ekafrozen@users.noreply.github.com>
2024-05-26 20:08:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
 provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
-  s3_force_path_style         = true
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  endpoint                    = "http://localhost:4566"
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-
-  tags = {
-    Name        = var.bucket_name
-    Environment = "local"
-  }
+  region = "us-east-1"
 }
