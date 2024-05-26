4e3ad8d98a51cfb11a20a3ca21b8d197b7a370d6
Kamil Gatkowski <152792497+ekafrozen@users.noreply.github.com>
2024-05-26 20:44:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 resource "aws_s3_bucket" "my_bucket" {
+  provider = aws.localstack
   bucket = "my-bucket-name"
 }
 
 resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  provider = aws.localstack
   bucket = aws_s3_bucket.my_bucket.id
   acl    = "private"
 }
