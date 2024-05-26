25f7be83c6e354fd80a1ad9a3b3f71b1ba08c6d2
Kamil Gatkowski <152792497+ekafrozen@users.noreply.github.com>
2024-05-26 20:39:57 +0200

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  alias                      = "localstack"
+  access_key                 = "test"
+  region                     = "eu-central-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
