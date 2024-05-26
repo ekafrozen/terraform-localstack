a1ed55f2ae1e5d031fc71692e74a966ad76aeb15
Kamil Gatkowski <152792497+ekafrozen@users.noreply.github.com>
2024-05-26 20:21:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "my_bucket" {
+}
+
 resource "aws_s3_bucket_acl" "my_bucket_acl" {
   bucket = aws_s3_bucket.my_bucket.id
 }
+
 
