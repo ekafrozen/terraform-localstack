3a70c345f971ab5fee729a7775d70e65a64e12b6
Kamil Gatkowski <152792497+ekafrozen@users.noreply.github.com>
2024-05-26 20:10:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
+}
